3a8db2894707a4075b6ba2da8ca7477666ecdc8c
@christophe.henry2 <christophe.henry2@soprabanking.com>
2022-05-24 14:21:40 +0200

    build: Add Mac automatic build and setup jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,26 +21,14 @@ plugins{
 	jacoco
 }
 
+tasks.jacocoTestReport {
+	dependsOn(tasks.findByName("jvmTest"))
+}
+
 apply<KorgeGradlePlugin>()
 
 korge {
 	group = "com.jackcat13.rogueLike"
 	id = "myDailyApocalypse"
-
-// To enable all targets at once
-
-	//targetAll()
-
-// To enable targets based on properties/environment variables
-	//targetDefault()
-
-// To selectively enable targets
-	
-	targetJvm()
-	targetJs()
-	targetDesktop()
-	targetIos()
-	targetAndroidIndirect() // targetAndroidDirect()
-	//targetAndroidDirect()
-
+	targetAll()
 }
