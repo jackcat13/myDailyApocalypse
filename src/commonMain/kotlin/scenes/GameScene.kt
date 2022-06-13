652f4ef35ed53d2237aa5eee97c21ca84c773a5d
jackCat13 <jackCat13@users.noreply.github.com>
2022-06-13 11:54:57 +0200

    feat: procedural regeneration of the world when accessing 'farest' chunks implemented. Simulating infinite and changing world

--- src/commonMain/kotlin/scenes/GameScene.kt
+++ src/commonMain/kotlin/scenes/GameScene.kt
@@ -5,29 +5,29 @@ import com.soywiz.korge.resources.resourceBitmap
 import com.soywiz.korge.scene.Scene
 import com.soywiz.korge.view.Container
 import com.soywiz.korge.view.addUpdater
-import com.soywiz.korge.view.camera
 import com.soywiz.korge.view.camera.cameraContainer
-import com.soywiz.korge.view.centerOn
-import com.soywiz.korge.view.fixedSizeContainer
-import com.soywiz.korge.view.position
-import com.soywiz.korge.view.solidRect
-import com.soywiz.korim.color.Colors
 import com.soywiz.korio.resources.ResourcesContainer
+import config.GameConfig.chunksSize
 import entities.Player
 import extensions.checkMoves
 import extensions.generateWorld
+import extensions.worldLoadingCheck
 import module.MainModule
 
 val ResourcesContainer.player_png by resourceBitmap("player.png")
 val ResourcesContainer.background_texture by resourceBitmap("grass.png")
-val world = World()
+var world = World()
 
 class GameScene: Scene() {
     private val player = Player()
     override suspend fun Container.sceneInit() {
         val camera = cameraContainer(MainModule.virtualWidth.toDouble(), MainModule.virtualHeight.toDouble()) {
             generateWorld(world, background_texture)
-            player.initDraw(this, 0.0, 0.0, player_png.get(), Player::class.simpleName.toString())
+            player.initDraw(this, -chunksSize, -chunksSize, player_png.get(), Player::class.simpleName.toString())
+            player.speed = 50.0 //TODO: for test only, TO REMOVE
+            addUpdater {
+                world = worldLoadingCheck(world, player, background_texture)
+            }
         }
         addUpdater {
             checkMoves(player)
